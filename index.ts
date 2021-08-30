4d93c7b0ba95a5da30ef9b2ab9d266f8bbc43b6d
Isaque <iskschluter@gmail.com>
2021-08-30 14:38:29 -0300

    fix PORT

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import fipeHandler from './routes/fipe';
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 80;
 
 app.get('/:placa', fipeHandler);
 
